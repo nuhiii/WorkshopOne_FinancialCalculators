3719f3b3de994f135b9663f54155d147ef919989
Nuhirath Rafthia <nr2793@columbia.edu>
2024-04-12 12:11:54 -0400

    set up classes for the three different calculators

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,6 +1,12 @@
 package com.ps;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        // Calculator One
+//        MortgageCalculator mortgageCalculator = new MortgageCalculator();
+//        // Calculator Two
+//        FutureValue futureValue = new FutureValue();
+//        // Calculator Three
+//        PresentValue presentValue = new PresentValue();
+        System.out.println("No Errors! Successful Execution!");
     }
 }
